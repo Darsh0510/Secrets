13ca66294bea2edfdb16fe043e5dbe21949edf2a
Darshan Vemula <darshan2002vemula@gmail.com>
2023-08-18 12:46:07 +0530

    Level 4 security and bcrypt hashing

--- app.js
+++ app.js
@@ -6,7 +6,8 @@ const ejs = require('ejs')
 const connectDB = require('./db/connect')
 const User = require('./models/user')
 const bodyParser = require('body-parser')
-const md5 = require('md5')
+const bcrypt = require('bcrypt');
+const saltRounds = 10;
 
 
 app.use(bodyParser.urlencoded({extended:true}))
@@ -29,34 +30,47 @@ app.get('/register',(req,res)=>{
 })
 
 app.post('/register',async (req,res)=>{
-    const newUser = new User({
-        email: req.body.username,
-        password: md5(req.body.password) 
-    })
-
-    await newUser.save().then((doc)=>{
-        
-        res.render(('secrets'))
-    }).catch((err)=>{
-        console.log(err)
-    })
+    bcrypt.hash(req.body.password, saltRounds,async function(err, hash) {
+        const newUser = new User({
+            email: req.body.username,
+            password:  hash
+        })
+    
+        await newUser.save().then((doc)=>{
+            
+            res.render(('secrets'))
+        }).catch((err)=>{
+            console.log(err)
+        })
+    });
+    
 })
 
 app.post("/login",async (req,res)=>{
     const username = req.body.username
-    const password = md5(req.body.password)
+    const password = req.body.password
 
     const found = await User.findOne({email: username})
     if (!found){
         throw new Error("No Match")
     }
 
-    if (found.password === password){
-        res.render('secrets')
-    }
-    else{
-        throw new Error("No Authentication")
-    }
+    bcrypt.compare(password, found.password, function(err, result) {
+        // result == true
+        if (result ==true){
+            res.render('secrets')
+        }else{
+            throw new Error("No Authentication")
+        }
+        
+           
+       
+        
+        
+    });
+        
+    
+    
 })
 
 const port = 3000
